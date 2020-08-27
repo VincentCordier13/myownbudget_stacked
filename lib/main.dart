b49401da1b9bed5e50e63a78562a305b1aad3421
VincentCordier13 <vincent.cordier.13@gmail.com>
2020-08-27 16:23:24 +0200

    test git function

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Container(),
+      home: Container(),//test git
     );
   }
 }
